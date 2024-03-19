625ad7a16f89a7ed230c5d41bbc9faeeeee4dabf
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-03-19 09:10:01 +0300

    Included a css feature from hypercolor.dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return (
-    <Button>Click me</Button>
-  );
+export default async function Home() {
+  return(
+    <div className="w-screen min-h-screen bg-gradient-to-t from-gray-900 via-gray-100 to-gray-900">
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2"></div>
+    </div>
+  )
 }
