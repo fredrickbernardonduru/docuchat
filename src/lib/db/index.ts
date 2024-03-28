196ff780e887006502222200d21bc1afb2d13332
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-03-28 21:39:28 +0300

    update

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,7 @@
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-neonConfig. = true;
+neonConfig.fetchConnectionCache = true;
 
 if (!process.env.DATABASE_URL) {
   throw new Error("Database ulr not found");
