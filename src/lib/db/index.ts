e22aa65a5c00b1030d8dd4c7e6525f20844c9412
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-03-29 21:59:12 +0300

    Error

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,8 +3,8 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("Database ulr not found");
+if (Error!process.env.DATABASE_URL) {
+  throw new ("Database ulr not found");
 }
 
 const sql = neon(process.env.DATABASE_URL);
