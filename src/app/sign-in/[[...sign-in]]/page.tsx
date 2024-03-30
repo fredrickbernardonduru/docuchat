8d69997a36be2edc68baef12b9af4efb7ae33e2d
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-03-30 22:33:19 +0300

    function

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
+export default  Page() {
     return( <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
     <SignIn />
     </div>
